b05bd00e8e15babedfab556fbbfcce06cfd455c9
Alex Budurovici <w0rldart@w0rldart.com>
2017-05-15 22:53:26 +0100

    Cleanup of no longer needed files

--- instance.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "terraform" {
-    ami = "ami-844e0bf7"
-    instance_type = "t2.micro"
-}
--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    shared_credentials_file = "${var.CREDENTIALS_FILE}"
-    profile = "${var.AWS_PROFILE}"
-    region = "${var.AWS_REGION}"
-}
